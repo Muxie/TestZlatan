cf5478921515cfc4eb5473f8c5785df637f05486
Hampus <ponised@gmail.com>
2016-01-28 14:43:54 +0100

    Initial checking of sales tools

---
+++ LocalSalesTools/SalesTools/src/ca/demo/salestools/Main.java
@@ -0,0 +1,16 @@
+package ca.demo.salestools;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Salesdata data = new Salesdata();
+		displayGreeting();
+		data.display();
+	}
+	private static void displayGreeting()
+	{
+		System.out.println("Hello happy sales people!");
+		System.out.println("This app shows sales data");
+	}
+}
